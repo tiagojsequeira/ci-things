1a59d7401079ab4fe659cceb8f01f59599ecabcd
tiagojsequeira <mr_tiagosequeira@sapo.pt>
2017-07-31 18:39:39 +0100

    Update Dashing.groovy
    update buildTime

--- scr/com/company/Dashing.groovy
+++ scr/com/company/Dashing.groovy
@@ -9,9 +9,9 @@ class Dashing implements Serializable {
       this.script = script
     }
 
-    def notify(buildResult, branchName, commitAuthor) {
-      def String payload = URLEncoder.encode('{ "buildResult":"'+buildResult+'\","branchName":\"'+branchName+'\","commitAuthor":\"'+commitAuthor+'\"}','UTF-8')
-      script.httpRequest "http://192.168.5.201:8080/webhook.php?name=LastBuildAndroid&payload="+payload
+    def notify(buildResult, branchName, commitAuthor ,time) {
+      def String payload = URLEncoder.encode('{ "buildResult":"'+buildResult+'\","branchName":\"'+branchName+'\","commitAuthor":\"'+commitAuthor+'\","buildTime":\"'+time+'\"}','UTF-8')
+      script.httpRequest "http://192.168.5.201:8080/webhook.php?name=LastBuildiOS&payload="+payload
     }
 
 }
